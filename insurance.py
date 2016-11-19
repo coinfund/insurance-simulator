dd7728aab88cacd5532b54e567ace218ebbf3547
Jake Brukhman <jbrukh@gmail.com>
2016-11-19 15:36:30 -0500

    better outputs

--- insurance.py
+++ insurance.py
@@ -6,12 +6,12 @@ from random import random
 
 class Estimator():
   """
-  Simple estimator for calculating premiums and/or payouts
-  for a basic Bernoulli distribution based on:
+  Estimator for calculating premiums and/or payouts for a 
+  basic Bernoulli distribution insurance model based on:
 
-    https://static1.squarespace.com/static/53442a39e4b06157f4464de8/t/582e8dcdcd0f688e8f4d53c3/1479445965306/Decentralized_Insurance_Scheme.pdf
+    * http://bit.ly/2g8fj7r
+  
   """
-
   def __init__(self, p, n=0, pi=.9999, P=None, P0=None):
     """
       Inputs:
@@ -88,6 +88,7 @@ class InsurancePool():
     self.n = 0
     self.p = p
     self.P = P
+    self.L = 0
     self.cap = seed
     self.claims  = 0
     self.issued  = 0
@@ -109,31 +110,35 @@ class InsurancePool():
     estimator = Estimator(self.p, n=eff_n, P=self.P)
     self.cap += estimator.P0
     self.inbound += estimator.P0
-    print('selling policy at %0.2f, k = %s, effective capitalization supports k = %d' % (estimator.P0, estimator.k, self.cap / self.P))
+    print('* issue policy @ $%0.2f,\n\t\tk = %s\n\t\teffective capitalization supports k = %d\n' % \
+      (estimator.P0, estimator.k, self.cap / self.P))
     self.L = self.n * self.P
 
   def claim(self):
-    print('policy is claimed')
+    print('* policy is claimed')
     self.n -= 1
     self.cap -= self.P
     self.claims += 1
     if self.cap < 0:
-      raise Exception('pool is insolvent')
+      raise Exception('* pool is insolvent')
 
   def expire(self):
-    print('policy is expired')
+    print('* policy is expired')
     self.n -= 1
 
   def __str__(self):
+
     return """
       n:       %s
       P:       %s
       cap:     %s
       L:       %s
 
-      issued:  %-4d   (pool = %0.2f / L = %0.2f -> %0.2f pct)
-      claims:  %-4d   (%0.2f)
-    """ % (self.n, self.P, self.cap, self.L, self.issued, self.inbound, self.issued*self.P, (self.inbound / (self.issued*self.P) * 100), self.claims, self.claims*self.P)
+      policies issued:   %-4d
+      total $ collected: $%0.2f
+      claims:            %-4d
+      payouts:           $%0.2f
+    """ % (self.n, self.P, self.cap, self.L, self.issued, self.inbound, self.claims, self.claims*self.P)
 
 def start(p = 0.05, P=500):
   """
@@ -142,9 +147,6 @@ def start(p = 0.05, P=500):
 
   pool = InsurancePool(p, P, seed=20000)
   
-  # Issue 1000 policies
-  for i in range(1000):
-    pool.issue()
   
   # Randomly issue more policies or expire/claim
   # existing ones...
@@ -154,10 +156,11 @@ def start(p = 0.05, P=500):
       pool.issue()
     else:
       rnd = random()
-      if rnd < p:
-        pool.claim()
-      else:
-        pool.expire()
+      if pool.n > 0:
+        if rnd < p:
+          pool.claim()
+        else:
+          pool.expire()
       print(pool)
 
   # Redeem or expire policies until pool
@@ -171,9 +174,9 @@ def start(p = 0.05, P=500):
     print(pool)
 
   print("""
-    excess capital: %0.2f
-    payouts:        %d
-    payouts value:  %0.2f
+    excess capital:  $%0.2f
+    payouts:         %d
+    paid out value:  $%0.2f
   """ % (pool.cap, pool.claims, pool.claims * pool.P))
 
 if __name__ == '__main__':
